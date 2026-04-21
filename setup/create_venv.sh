613ab130156b85b1ce461b7479944441f53cbcc8
mpmunch <mpmunch@gmail.com>
2026-04-21 10:16:32 +0200

    Add activation of virtual environment and patching script to create_venv.sh

--- setup/create_venv.sh
+++ setup/create_venv.sh
@@ -10,3 +10,11 @@ srun singularity exec --nv \
      /bin/bash -c "export TMPDIR=/scratch/singularity/tmp && \
                    source /scratch/my_venv/bin/activate && \
                    pip install -r requirements.txt --no-cache-dir"
+
+
+# Activate the virtual environment and run patch_mistral.sh to patch transformers with custom modeling_mistral.py (bidirectional attention).
+srun singularity exec --nv \
+     -B my_venv:/scratch/my_venv \ \
+     /ceph/container/python/python_3.10.sif \
+     /bin/bash -c "source /scratch/my_venv/bin/activate && \
+                   bash scripts/patch_mistral.sh"
